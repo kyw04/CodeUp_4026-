e5d73aaf8765b582190aaf78d86fd1cdb8630c1b
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-05-27 23:27:34 +0900

    초기 커밋.

---
+++ CodeUp_4026 중앙 값/소스.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int arr[5];
+	for (int i = 0; i < 5; i++)
+		cin >> arr[i];
+	sort(arr, arr + 5);
+	cout << arr[2];
+	return 0;
+}
